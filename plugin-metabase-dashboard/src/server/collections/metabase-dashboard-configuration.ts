4e8865b81f77fe991a9639959f744ddc3f1e0bfa
cae545e5 <1876869218@qq.com>
2025-05-19 11:09:44 +0800

    feat: 新增字段expiration

--- plugin-metabase-dashboard/src/server/collections/metabase-dashboard-configuration.ts
+++ plugin-metabase-dashboard/src/server/collections/metabase-dashboard-configuration.ts
@@ -21,5 +21,11 @@ export default defineCollection({
       name: 'secretKey',
       required: true,
     },
+    {
+      type: 'integer',
+      name: 'expiration',
+      defaultValue: 10, // 默认10分钟，单位为分钟
+      description: 'JWT令牌过期时间(分钟)'
+    },
   ],
 });
